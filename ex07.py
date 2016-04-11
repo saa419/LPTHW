d9adc1f0a274f885facefacadada2f3df3c173a4
Santiago A <saa419@stern.nyu.edu>
2016-04-10 18:32:07 -0700

    Initial creation
    Learn python the Hard Way repo. Ex. 1-9 already completed

---
+++ ex07.py
@@ -0,0 +1,27 @@
+# Print a line
+print "Mary had a little lamb."
+# Print a line with a string at the end
+print "Its fleece was white as %s." % 'snow'
+# Print a line
+print "And everywhere that Mary went."
+# Print a dot 10 times
+print "." * 10 # what'd that do?
+
+# Create a variable for each letter
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# Print all the variables together. the comma at the end makes it continue printing on the same line
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
