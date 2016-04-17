64634e2549a7e0a644a5a7734dfb6f341d3f38c3
Santiago A <santiago.a.andujar@gmail.com>
2016-04-17 15:52:42 -0700

    Completed lessons 22 & 24

---
+++ ex22.py
@@ -0,0 +1,34 @@
+'''
+print: prints the variable or string that follows
++: Adds or concatenates two things
+/: divides two things
+-: subtracts two things
+*: multiplies or repeats thing 1 by thing 2
+%: modulo: provides the remainder of a division. Also used for placing strings and numbers into statements
+<: less than
+>: greater than
+>=: greater than or equal to
+<=: less than or equal to
+#: Makes a comment
+=: assigns a value to a variable
+%s: represents a string variable
+%d: represents a digit variable
+%r: represents a raw data variable
+\n: new line
+\t: tab
+\\: one backslash
+""": block of text
+int: makes something an integer
+raw_input: takes the raw input from the user and passes it to something
+from: from a particular module,
+import: import something from somewhere else to be used in the program
+open: open a file
+read: read a file
+close: close a file
+truncate: truncates (erases) a file
+write: writes to a file
+def: defines a function
+while: while something is true, do something
++=: add something to an existing variable, make that the new variable
+
+'''
